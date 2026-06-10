47a5159a57d7c48ff946e9844d23c866cc2ca12d
AhmedMedhat <75226176+AhmedMedhat77@users.noreply.github.com>
2026-06-10 22:34:21 +0300

    feat: add footer component and integrate it into the app layout with sticky positioning

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class Footer {}
--- src/app/layouts/app-layout/app-layout.ts
+++ src/app/layouts/app-layout/app-layout.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [Navbar],
+  imports: [Navbar, Footer],
   templateUrl: './app-layout.html',
-  styles: ``,
+  styles: `
+    :host {
+      display: flex;
+      flex-direction: column;
+      min-height: 100vh;
+    }
+  `,
 })
 export class AppLayout {}
