e2e34eee227f23883a2f009ff0e1b12c208c3a06
AhmedMedhat <75226176+AhmedMedhat77@users.noreply.github.com>
2026-06-10 16:34:48 +0300

    refactor: extract product display logic into new ProductsList component and update Home screen layout

--- src/app/screens/home/home.ts
+++ src/app/screens/home/home.ts
@@ -1,41 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CategiesList } from '../../components/categies-list/categies-list';
-import { ProductCard } from '../../components/product-card/product-card';
-import { SkeltonProductCard } from '../../components/skelton-product-card/skelton-product-card';
-import { Products } from '../../services/products';
+import { ProductsList } from '../../components/products-list/products-list';
 
 @Component({
   selector: 'app-home',
-  imports: [ProductCard, SkeltonProductCard, CategiesList],
+  imports: [CategiesList, ProductsList],
   templateUrl: './home.html',
   styles: ``,
 })
-export class Home implements OnInit {
-  private productService = inject(Products);
-
-  products = this.productService.products;
-  loading = this.productService.loading;
-  loadingNext = this.productService.loadingNext;
-  totalProducts = this.productService.totalProducts;
-  hasMore = this.productService.hasMore;
-  skeletonItems = this.productService.skeletonItems;
-
-  ngOnInit(): void {
-    this.productService.fetchProducts();
-    this.setupScrollListener();
-  }
-
-  private setupScrollListener(): void {
-    window.addEventListener('scroll', () => {
-      if (this.loading() || this.loadingNext() || !this.hasMore()) return;
-
-      const threshold = 400;
-      const position = window.innerHeight + window.scrollY;
-      const bottom = document.body.offsetHeight - threshold;
-
-      if (position >= bottom) {
-        this.productService.fetchNext();
-      }
-    });
-  }
-}
+export class Home {}
