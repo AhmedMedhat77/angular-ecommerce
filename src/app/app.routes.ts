78a61f848303d74f977c6bbe7191a90de927fe3f
AhmedMedhat <75226176+AhmedMedhat77@users.noreply.github.com>
2026-06-10 16:40:51 +0300

    feat: implement cart screen and add route configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { About } from './screens/about/about';
+import { Cart } from './screens/cart/cart';
 import { Home } from './screens/home/home';
 
 export const routes: Routes = [
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'about',
     component: About,
+  },
+  {
+    path: 'cart',
+    component: Cart,
   },
 ];
